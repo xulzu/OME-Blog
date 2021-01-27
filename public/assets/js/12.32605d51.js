2edeecff3ecfba29d6fbb775435ad32c2d482995
ome <xulzu@qq.com>
2021-01-28 00:40:05 +0800

    build

--- public/assets/js/12.32605d51.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{246:function(n,t,e){"use strict";e.r(t);var s=e(10),i=Object(s.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n\t关于\n\t\n")])}),[],!1,null,null,null);t.default=i.exports}}]);
